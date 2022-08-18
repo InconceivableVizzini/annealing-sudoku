599099527c7e6d23a7f35363ef93eae749d9f8b7
jmpeax <jmpeax@jmpeax.net>
2022-08-18 03:53:12 -0400

    Add STC containers in their own compilation unit

---
+++ src/containers.c
@@ -0,0 +1,11 @@
+#define STC_IMPLEMENTATION
+
+#include <stc/cstr.h>
+
+#define i_val uint_fast8_t
+#define i_tag u8
+#include <stc/carr2.h>
+
+#define i_val uint_fast8_t
+#define i_tag u8
+#include <stc/clist.h>
